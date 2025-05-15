6a5641a7429e91cba87ac154469d4a732a439517
Ander Ayesta <ander.ayesta@opendeusto.es>
2025-05-15 20:47:20 +0200

    Creacion de todas las clases en el .h y implementacion de producto.cpp

---
+++ Proyecto_grupo14_C++/src/Producto.cpp
@@ -0,0 +1,107 @@
+#include "Producto.h"
+#include <string.h>
+#include <iostream>
+
+Producto::Producto() {
+	this->idProd = 0;
+	this->nombreProd = NULL;
+	this->precio = 0;
+	this->codSeccion = 0;
+	this->codProveedor = 0;
+	this->tipo = NULL;
+	this->numDisponibles = 0;
+}
+
+Producto::Producto(int idProd, const char *nombreProd, float precio, int codSeccion, int codProveedor, const char *tipo, int numDisponibles) {
+	this->idProd = idProd;
+	this->nombreProd = new char[strlen(nombreProd)+1];
+	strcpy(this->nombreProd, nombreProd);
+	this->precio = precio;
+	this->codSeccion = codSeccion;
+	this->codProveedor = codProveedor;
+	this->tipo = new char[strlen(tipo)+1];
+	strcpy(this->tipo, tipo);
+	this->numDisponibles = numDisponibles;
+}
+
+Producto::Producto(const Producto &p) {
+	this->idProd = p.idProd;
+	this->nombreProd = new char[strlen(p.nombreProd)+1];
+	strcpy(this->nombreProd, p.nombreProd);
+	this->precio = p.precio;
+	this->codSeccion = p.codSeccion;
+	this->codProveedor = p.codProveedor;
+	this->tipo = new char[strlen(p.tipo)+1];
+	strcpy(this->tipo, p.tipo);
+	this->numDisponibles = p.numDisponibles;
+}
+
+Producto::~Producto() {
+	delete [] nombreProd;
+	delete [] tipo;
+}
+
+int Producto::getIdProd() {
+	return this->idProd;
+}
+
+const char *Producto::getNombreProd() {
+	return this->nombreProd;
+}
+
+float Producto::getPrecio() {
+	return this->precio;
+}
+
+int Producto::getCodSeccion() {
+	return this->codSeccion;
+}
+
+int Producto::getCodProveedor() {
+	return this->codProveedor;
+}
+
+const char *Producto::getTipo() {
+	return this->tipo;
+}
+
+int Producto::getNumDisponibles() {
+	return this->numDisponibles;
+}
+
+void Producto::setIdProd(int idProd) {
+	this->idProd = idProd;
+}
+
+void Producto::setNombreProd(const char *nombreProd) {
+	if(this->nombreProd != NULL) {
+		delete [] this->nombreProd;
+		this->nombreProd = new char[strlen(nombreProd)+1];
+		strcpy(this->nombreProd, nombreProd);
+	}
+}
+
+void Producto::setPrecio(float precio) {
+	this->precio = precio;
+}
+
+void Producto::setCodSeccion(int codSeccion) {
+	this->codSeccion = codSeccion;
+}
+
+void Producto::setCodProveedor(int codProveedor) {
+	this->codProveedor = codProveedor;
+}
+
+void Producto::setTipo(const char *tipo) {
+	if (this->tipo != NULL) {
+		delete[] this->tipo;
+	}
+	this->tipo = new char[strlen(tipo) + 1];
+	strcpy(this->tipo, tipo);
+}
+
+void Producto::setNumDisponibles(int numDisponibles) {
+	this->numDisponibles = numDisponibles;
+}
+
---
+++ Proyecto_grupo14_C++/src/Producto.h
@@ -0,0 +1,127 @@
+
+
+#ifndef SRC_PRODUCTO_H_
+#define SRC_PRODUCTO_H_
+
+class Producto {
+protected:
+	int idProd;
+	char *nombreProd;
+	float precio;
+	int codSeccion;
+	int codProveedor;
+	char *tipo;
+	int numDisponibles;
+public:
+	Producto();
+	Producto(int idProd, const char *nombreProd, float precio, int codSeccion, int codProveedor, const char *tipo, int numDisponibles);
+	Producto(const Producto &p);
+	virtual ~Producto();
+
+	int getIdProd();
+	const char *getNombreProd();
+	float getPrecio();
+	int getCodSeccion();
+	int getCodProveedor();
+	const char *getTipo();
+	int getNumDisponibles();
+
+	void setIdProd(int idProd);
+	void setNombreProd(const char * nombre);
+	void setPrecio(float precio);
+	void setCodSeccion(int codSeccion);
+	void setCodProveedor(int codProveedor);
+	void setTipo(const char * tipo);
+	void setNumDisponibles(int numDisponibles);
+};
+
+class Proveedor {
+protected:
+	int codProveedor;
+	char *nombreProveedor;
+	int codPostal;
+	char *contrasena;
+public:
+	Proveedor();
+	Proveedor(int codProveedor, const char *nombreProveedor, int codPostal, const char *contrasena);
+	Proveedor(const Proveedor &p);
+	virtual ~Proveedor();
+
+	int getCodProveedor();
+	const char *getNombreProveedor();
+	int getCodPostal();
+	const char *getContrasena();
+
+	void setCodProveedor(int codProveedor);
+	void setNombreProveedor(const char *nombreProveedor);
+	void setCodigoPostal(int codPostal);
+	void setContrasena(const char *contrasena);
+
+
+};
+
+class Seccion {
+protected:
+	int codSeccion;
+	char *nombreSeccion;
+public:
+	Seccion();
+	Seccion(int codSeccion, const char *nombreSeccion);
+	Seccion(const Seccion &s);
+	~Seccion();
+
+	int getCodSeccion();
+	const char *getNombreSeccion();
+
+	void setCodSeccion(int codSeccion);
+	void setNombreSeccion(const char *nombreSeccion);
+};
+
+class Departamento {
+protected:
+	int idDepartamento;
+	char *nombreDepartamento;
+	int NSSJefe;
+public:
+	Departamento();
+	Departamento(int idDepartamento, const char *nombreDepartamento, int NSSJefe);
+	Departamento(const Departamento &d);
+	~Departamento();
+
+	int getIdDepartamento();
+	const char *getNombreDepartamento();
+	int getNSSJefe();
+
+	void setIdDepartamento(int idDepartamento);
+	void setNombreDepartamento(const char *nombreDepartamento);
+	void setNSSJefe(int NSSJefe);
+};
+
+class Empleado {
+protected:
+	int NSS;
+	char *nombreEmpleado;
+	char *contrasena;
+	int codSeccion;
+	int idDepartamento;
+public:
+	Empleado();
+	Empleado(int NSS, const char *nombreEmpleado, const char *contrasena, int codSeccion, int idDepartamento);
+	Empleado(const Empleado &e);
+	~Empleado();
+
+	int getNSS();
+	const char *getNombreEmpleado();
+	const char *getContrasena();
+	int getCodSeccion();
+	int getIdDepartamento();
+
+	void setNSS(int NSS);
+	void setNombreEmpleado(const char *nombreEmpleado);
+	void setContrasena(const char *contrasena);
+	void setCodSeccion(int codSeccion);
+	void setIdDepartamento(int idDepartamento);
+};
+
+
+#endif /* SRC_PRODUCTO_H_ */
