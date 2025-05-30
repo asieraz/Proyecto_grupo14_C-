df732d9599c959c90198fb539cc1a8bbfd044f16
Jon Fernández de Cea <j.fernandezde@opendeusto.es>
2025-05-30 21:40:20 +0200

    Server Final.

--- Proyecto_grupo14_C++/src/ServerSocket/server.cpp
+++ Proyecto_grupo14_C++/src/ServerSocket/server.cpp
@@ -170,7 +170,7 @@ void run_server(sqlite3 *db, char* ip_server) {
             }
 
         } else if (strcmp(recvBuff, "LOGIN-PROVEEDOR") == 0) {
-        	const char *mensaje = "Proveedor, ntroduce tu nombre y apellido (Separado por un espacio):\n";
+        	const char *mensaje = "Proveedor, introduce tu nombre y apellido (Separado por un espacio):\n";
         	send(comm_socket, mensaje, strlen(mensaje) + 1, 0); // incluye \0
 
         	memset(recvBuff, 0, sizeof(recvBuff));
